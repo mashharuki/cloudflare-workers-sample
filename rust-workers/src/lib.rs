646fdad44c60b3fafc3cc04cea3de75a8931e339
mashharuki <avp.104.106.107@ae.auone-net.jp>
2026-02-16 12:15:01 +0900

    update

--- rust-workers/src/lib.rs
+++ rust-workers/src/lib.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use worker::*;
+
+// ルートハンドラー
+fn root_handler(
+    _req: worker::Request, 
+    _ctx: RouteContext<()>
+) -> worker::Result<worker::Response> {
+    // "Hello World!" を返す
+    Response::ok("Hello World!")
+}
+
+// エントリーポイントとなるメイン関数
+#[event(fetch)]
+async fn main(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+    // ルーターの設定
+    let router = Router::new();
+
+    // ルーティングの設定
+    router.get("/", root_handler).run(_req, _env).await
 }
